2426742e9961a5e206f622136631b5047060a42c
Dhaval R Somaiya <dhavalsom@gmail.com>
2018-01-23 01:16:06 +0530

    Removed App.config from DAL Library and included connection string in web.config

--- OMCApi/OMC.DAL.Library/SignInDataAccess.cs
+++ OMCApi/OMC.DAL.Library/SignInDataAccess.cs
@@ -25,15 +25,22 @@ namespace OMC.DAL.Library
         {
             try
             {
-                SqlConnection con = new SqlConnection( WebConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString);
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString);
+                //SqlConnection con = new SqlConnection("Data Source=localhost;Initial Catalog=HealthCare;Integrated Security=True;");
                 SqlCommand com = new SqlCommand("select * from [dbo].[UserDetail] where firstname = '" + user.Username + "' and password = '" + user.Password + "'", con);
                 com.CommandType = CommandType.Text;
                 SqlDataAdapter da = new SqlDataAdapter(com);
                 DataTable dt = new DataTable();
+                DataSet ds = new DataSet();
                 con.Open();
-                da.Fill(dt);
+                da.Fill(ds);
+                int count = ds.Tables[0].Rows.Count;
                 con.Close();
-                return true;
+
+                if (count == 1)
+                    return true;
+                else
+                    return false;
             }
             catch(Exception ex)
             {
